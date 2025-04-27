fe57b4304b521161d138055a83ae287bb33835aa
assia <assiabenmohamed>
2025-04-27 01:42:44 +0100

    fix project v2

--- gym-app-frontend/src/components/vitrine/Pricing.tsx
+++ gym-app-frontend/src/components/vitrine/Pricing.tsx
@@ -2,8 +2,8 @@
 import Image from "next/image";
 import React, { useEffect } from "react";
 import { FaCheck } from "react-icons/fa";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const gymOffers = [
   {
