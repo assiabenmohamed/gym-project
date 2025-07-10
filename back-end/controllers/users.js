a6fd4ddf31e96b6ab80a08726cc7050059b03f1e
assia <assiabenmohamed>
2025-07-10 01:42:46 +0100

    gym v6

--- back-end/controllers/users.js
+++ back-end/controllers/users.js
@@ -62,7 +62,9 @@ export async function register(req, res) {
     res.status(201).json({ user: userResponse });
   } catch (error) {
     console.log(error);
-    res.json({ message: "error in register controller" });
+    res
+      .status(500)
+      .json({ message: "error in register controller", error: error.message });
   }
 }
 
