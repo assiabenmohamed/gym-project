806b0dd48ef0fb2b9518973405b7b7dbdfe424d5
assia <assiabenmohamed>
2025-07-30 04:36:22 +0100

    fix: autoriser domaine image onrender

--- gym-app-frontend/next.config.ts
+++ gym-app-frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["localhost"],
+    domains: ["gym-project-3m9w.onrender.com"],
   },
 };
 
